562d4d031a594a7550c211eff0adeeb08cc6f4cc
Ale Figueroa <yknx.4.b@gmail.com>
2018-02-22 14:40:09 -0600

    Fix build and test config

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,21 @@
+const env = process.env.NODE_ENV;
 module.exports = {
   testEnvironment: 'node',
   transform: {
-    '.(ts|tsx)': '<rootDir>/preprocessor.js'
+    '.(ts|tsx)': '<rootDir>/preprocessor.js',
   },
-  moduleFileExtensions: [
-    'ts',
-    'tsx',
-    'js',
-    'jsx',
-  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)x?$',
   coverageDirectory: 'coverage',
-  collectCoverageFrom: [
-    'src/**/*.{ts,tsx,js,jsx}',
-    '!src/**/*.d.ts',
-  ],
+  collectCoverageFrom: ['src/**/*.{ts,tsx,js,jsx}', '!src/**/*.d.ts'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10,
+    },
+  },
+  notify: env !== 'test',
+  notifyMode: 'failure-success',
 };
