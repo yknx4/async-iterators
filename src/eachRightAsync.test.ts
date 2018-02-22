9c86f5aeac1e69cbcc0827a38135c7d5760f3f4d
Ale Figueroa <yknx.4.b@gmail.com>
2018-02-22 16:18:57 -0600

    Fix test

--- src/eachRightAsync.test.ts
+++ src/eachRightAsync.test.ts
@@ -1,38 +1,38 @@
-import { eachAsync } from './eachAsync';
+import { eachRightAsync } from './eachRightAsync';
 
-describe('eachAsync', () => {
+describe('eachRightAsync', () => {
   const input = [1, 2, 3, 4, 5];
   it('should guarantee order and receive all iteratee params', async () => {
     const fn = jest.fn((i: number) => Promise.resolve(i).then(console.log));
-    await eachAsync(input, fn);
+    await eachRightAsync(input, fn);
     expect(fn.mock.calls).toEqual([
-      [1, 0, [1, 2, 3, 4, 5]],
-      [2, 1, [1, 2, 3, 4, 5]],
-      [3, 2, [1, 2, 3, 4, 5]],
-      [4, 3, [1, 2, 3, 4, 5]],
       [5, 4, [1, 2, 3, 4, 5]],
+      [4, 3, [1, 2, 3, 4, 5]],
+      [3, 2, [1, 2, 3, 4, 5]],
+      [2, 1, [1, 2, 3, 4, 5]],
+      [1, 0, [1, 2, 3, 4, 5]],
     ]);
   });
   it('should wait for async functions', async () => {
     const fn = jest.fn(async (i: number) => console.log(i));
-    await eachAsync(input, fn);
+    await eachRightAsync(input, fn);
     expect(fn.mock.calls).toEqual([
-      [1, 0, [1, 2, 3, 4, 5]],
-      [2, 1, [1, 2, 3, 4, 5]],
-      [3, 2, [1, 2, 3, 4, 5]],
-      [4, 3, [1, 2, 3, 4, 5]],
       [5, 4, [1, 2, 3, 4, 5]],
+      [4, 3, [1, 2, 3, 4, 5]],
+      [3, 2, [1, 2, 3, 4, 5]],
+      [2, 1, [1, 2, 3, 4, 5]],
+      [1, 0, [1, 2, 3, 4, 5]],
     ]);
   });
   it('should work for non-async functions', async () => {
     const fn = jest.fn(async (i: number) => console.log(i));
-    await eachAsync(input, fn);
+    await eachRightAsync(input, fn);
     expect(fn.mock.calls).toEqual([
-      [1, 0, [1, 2, 3, 4, 5]],
-      [2, 1, [1, 2, 3, 4, 5]],
-      [3, 2, [1, 2, 3, 4, 5]],
-      [4, 3, [1, 2, 3, 4, 5]],
       [5, 4, [1, 2, 3, 4, 5]],
+      [4, 3, [1, 2, 3, 4, 5]],
+      [3, 2, [1, 2, 3, 4, 5]],
+      [2, 1, [1, 2, 3, 4, 5]],
+      [1, 0, [1, 2, 3, 4, 5]],
     ]);
   });
   it('should resolve promises before returning', async () => {
@@ -44,13 +44,13 @@ describe('eachAsync', () => {
       }
     });
 
-    await eachAsync(input, fn);
+    await eachRightAsync(input, fn);
     expect(fn.mock.calls).toEqual([
-      [1, 0, [1, 2, 3, 4, 5]],
-      [2, 1, [1, 2, 3, 4, 5]],
-      [3, 2, [1, 2, 3, 4, 5]],
-      [4, 3, [1, 2, 3, 4, 5]],
       [5, 4, [1, 2, 3, 4, 5]],
+      [4, 3, [1, 2, 3, 4, 5]],
+      [3, 2, [1, 2, 3, 4, 5]],
+      [2, 1, [1, 2, 3, 4, 5]],
+      [1, 0, [1, 2, 3, 4, 5]],
     ]);
   });
 });
