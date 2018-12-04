f02a92d3fe6fb218aa12751b4a3b952387d66b67
[CircleCI] Ale Figueroa <afigueroa@goamv.com>
2018-12-04 14:01:53 -0600

    Fix weird jest bug

--- src/eachAsync.test.ts
+++ src/eachAsync.test.ts
@@ -1,56 +1,58 @@
-import { eachAsync } from './eachAsync';
+import { eachAsync } from './eachAsync'
 
 describe('eachAsync', () => {
-  const input = [1, 2, 3, 4, 5];
+  const input = [1, 2, 3, 4, 5]
   it('should guarantee order and receive all iteratee params', async () => {
-    const fn = jest.fn((i: number) => Promise.resolve(i).then(console.log));
-    await eachAsync(input, fn);
+    const fn = jest.fn((i: number) =>
+      Promise.resolve(i).then((i: number) => console.log(i))
+    )
+    await eachAsync(input, fn)
     expect(fn.mock.calls).toEqual([
       [1, 0, [1, 2, 3, 4, 5]],
       [2, 1, [1, 2, 3, 4, 5]],
       [3, 2, [1, 2, 3, 4, 5]],
       [4, 3, [1, 2, 3, 4, 5]],
-      [5, 4, [1, 2, 3, 4, 5]],
-    ]);
-  });
+      [5, 4, [1, 2, 3, 4, 5]]
+    ])
+  })
   it('should wait for async functions', async () => {
-    const fn = jest.fn(async (i: number) => console.log(i));
-    await eachAsync(input, fn);
+    const fn = jest.fn(async (i: number) => console.log(i))
+    await eachAsync(input, fn)
     expect(fn.mock.calls).toEqual([
       [1, 0, [1, 2, 3, 4, 5]],
       [2, 1, [1, 2, 3, 4, 5]],
       [3, 2, [1, 2, 3, 4, 5]],
       [4, 3, [1, 2, 3, 4, 5]],
-      [5, 4, [1, 2, 3, 4, 5]],
-    ]);
-  });
+      [5, 4, [1, 2, 3, 4, 5]]
+    ])
+  })
   it('should work for non-async functions', async () => {
-    const fn = jest.fn(async (i: number) => console.log(i));
-    await eachAsync(input, fn);
+    const fn = jest.fn(async (i: number) => console.log(i))
+    await eachAsync(input, fn)
     expect(fn.mock.calls).toEqual([
       [1, 0, [1, 2, 3, 4, 5]],
       [2, 1, [1, 2, 3, 4, 5]],
       [3, 2, [1, 2, 3, 4, 5]],
       [4, 3, [1, 2, 3, 4, 5]],
-      [5, 4, [1, 2, 3, 4, 5]],
-    ]);
-  });
+      [5, 4, [1, 2, 3, 4, 5]]
+    ])
+  })
   it('should resolve promises before returning', async () => {
     const fn = jest.fn((i: number) => {
       if (i % 2 === 0) {
-        return Promise.resolve(i).then(console.log);
+        return Promise.resolve(i).then((i: number) => console.log(i))
       } else {
-        return console.log(i);
+        return console.log(i)
       }
-    });
+    })
 
-    await eachAsync(input, fn);
+    await eachAsync(input, fn)
     expect(fn.mock.calls).toEqual([
       [1, 0, [1, 2, 3, 4, 5]],
       [2, 1, [1, 2, 3, 4, 5]],
       [3, 2, [1, 2, 3, 4, 5]],
       [4, 3, [1, 2, 3, 4, 5]],
-      [5, 4, [1, 2, 3, 4, 5]],
-    ]);
-  });
-});
+      [5, 4, [1, 2, 3, 4, 5]]
+    ])
+  })
+})
--- src/eachRightAsync.test.ts
+++ src/eachRightAsync.test.ts
@@ -1,56 +1,58 @@
-import { eachRightAsync } from './eachRightAsync';
+import { eachRightAsync } from './eachRightAsync'
 
 describe('eachRightAsync', () => {
-  const input = [1, 2, 3, 4, 5];
+  const input = [1, 2, 3, 4, 5]
   it('should guarantee order and receive all iteratee params', async () => {
-    const fn = jest.fn((i: number) => Promise.resolve(i).then(console.log));
-    await eachRightAsync(input, fn);
+    const fn = jest.fn((i: number) =>
+      Promise.resolve(i).then((i: number) => console.log(i))
+    )
+    await eachRightAsync(input, fn)
     expect(fn.mock.calls).toEqual([
       [5, 4, [1, 2, 3, 4, 5]],
       [4, 3, [1, 2, 3, 4, 5]],
       [3, 2, [1, 2, 3, 4, 5]],
       [2, 1, [1, 2, 3, 4, 5]],
-      [1, 0, [1, 2, 3, 4, 5]],
-    ]);
-  });
+      [1, 0, [1, 2, 3, 4, 5]]
+    ])
+  })
   it('should wait for async functions', async () => {
-    const fn = jest.fn(async (i: number) => console.log(i));
-    await eachRightAsync(input, fn);
+    const fn = jest.fn(async (i: number) => console.log(i))
+    await eachRightAsync(input, fn)
     expect(fn.mock.calls).toEqual([
       [5, 4, [1, 2, 3, 4, 5]],
       [4, 3, [1, 2, 3, 4, 5]],
       [3, 2, [1, 2, 3, 4, 5]],
       [2, 1, [1, 2, 3, 4, 5]],
-      [1, 0, [1, 2, 3, 4, 5]],
-    ]);
-  });
+      [1, 0, [1, 2, 3, 4, 5]]
+    ])
+  })
   it('should work for non-async functions', async () => {
-    const fn = jest.fn(async (i: number) => console.log(i));
-    await eachRightAsync(input, fn);
+    const fn = jest.fn(async (i: number) => console.log(i))
+    await eachRightAsync(input, fn)
     expect(fn.mock.calls).toEqual([
       [5, 4, [1, 2, 3, 4, 5]],
       [4, 3, [1, 2, 3, 4, 5]],
       [3, 2, [1, 2, 3, 4, 5]],
       [2, 1, [1, 2, 3, 4, 5]],
-      [1, 0, [1, 2, 3, 4, 5]],
-    ]);
-  });
+      [1, 0, [1, 2, 3, 4, 5]]
+    ])
+  })
   it('should resolve promises before returning', async () => {
     const fn = jest.fn((i: number) => {
       if (i % 2 === 0) {
-        return Promise.resolve(i).then(console.log);
+        return Promise.resolve(i).then((i: number) => console.log(i))
       } else {
-        return console.log(i);
+        return console.log(i)
       }
-    });
+    })
 
-    await eachRightAsync(input, fn);
+    await eachRightAsync(input, fn)
     expect(fn.mock.calls).toEqual([
       [5, 4, [1, 2, 3, 4, 5]],
       [4, 3, [1, 2, 3, 4, 5]],
       [3, 2, [1, 2, 3, 4, 5]],
       [2, 1, [1, 2, 3, 4, 5]],
-      [1, 0, [1, 2, 3, 4, 5]],
-    ]);
-  });
-});
+      [1, 0, [1, 2, 3, 4, 5]]
+    ])
+  })
+})
